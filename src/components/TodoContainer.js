bfa275fad4cebac7966df780dcafc7f3b98f6710
Nuri1977 <nurilacka@gmail.com>
2022-02-11 23:51:19 +0100

    Add styles to project

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -14,7 +14,7 @@ class TodoContainer extends Component {
         {
           id: uuidv4(),
           title: 'Setup development environment',
-          completed: true,
+          completed: false,
         },
         {
           id: uuidv4(),
@@ -64,14 +64,16 @@ class TodoContainer extends Component {
 
   render() {
     return (
-      <div>
-        <Header />
-        <InputTodo addTodoProps={this.addTodoItem} />
-        <TodoList
-          todos={this.state.todos}
-          handleChangeProps={this.handleChange}
-          deleteTodoProps={this.delTodo}
-        />
+      <div className="container">
+        <div className="inner">
+          <Header />
+          <InputTodo addTodoProps={this.addTodoItem} />
+          <TodoList
+            todos={this.state.todos}
+            handleChangeProps={this.handleChange}
+            deleteTodoProps={this.delTodo}
+          />
+        </div>
       </div>
     );
   }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,6 +3,7 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
+import styles from './TodoItem.module.css';
 
 class TodoItem extends React.Component {
   constructor(props) {
@@ -10,11 +11,30 @@ class TodoItem extends React.Component {
   }
 
   render() {
+    const completedStyle = {
+      fontStyle: 'italic',
+      color: '#595959',
+      opacity: 0.4,
+      textDecoration: 'line-through',
+    };
+    const { completed, id, title } = this.props.todo;
     return (
-      <li>
-        <input type="checkbox" checked={this.props.todo.comleted} onChange={() => this.props.handleChangeProps(this.props.todo.id)} />
-        {this.props.todo.title}
-        <button type="button" onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>Delete</button>
+      <li className={styles.item}>
+        <input
+          type="checkbox"
+          className={styles.checkbox}
+          checked={completed}
+          onChange={() => this.props.handleChangeProps(id)}
+        />
+        <span style={completed ? completedStyle : null}>
+          {title}
+        </span>
+        <button
+          type="button"
+          onClick={() => this.props.deleteTodoProps(this.props.todo.id)}
+        >
+          Delete
+        </button>
       </li>
     );
   }
